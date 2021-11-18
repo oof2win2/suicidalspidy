8e1824920c80c4f17d63262ef365ba97037388ff
oof2win2 <honza.koco44@gmail.com>
2021-11-18 23:12:34 +0100

    :construction: base research

---
+++ prototypes/legs.lua
@@ -0,0 +1,24 @@
+local lib = {}
+
+---Create spidertron legs
+function lib.create_leg (
+		spidertron_name,
+		scale,
+		initial_movement_speed,
+		movement_acceleration,
+		number,
+		animation
+	)
+	local legs = {}
+	-- TODO add in creation of spidy legs for a spidertron
+	for i = 1, number do
+		local 
+		table.insert(legs, {
+			
+		})
+	end
+
+	return legs
+end
+
+return lib
